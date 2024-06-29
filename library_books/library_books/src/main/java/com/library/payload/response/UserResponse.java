6b41b92e2e2f4e0e58dc5baa49c66624a9ca05e1
Priya Mittal <108448087+priya6397@users.noreply.github.com>
2024-06-29 14:33:07 +0530

    Add files via upload

---
+++ library_books/library_books/src/main/java/com/library/payload/response/UserResponse.java
@@ -0,0 +1,18 @@
+package com.library.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserResponse {
+    private Long id;
+    private String fullName;
+    private String address;
+    private String phoneNo;
+    private String email;
+}
